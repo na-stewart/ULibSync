9252026057c635e55fe1cfc06460d3b025c9c02a
sunset <aidanstewart@sunsetdeveloper.com>
2021-05-23 18:43:57 -0400

    ulib commands admin only

--- lua/ulx/modules/sh/sh_commands.lua
+++ lua/ulx/modules/sh/sh_commands.lua
@@ -2,7 +2,7 @@ function ulx.syncBans(calling_ply)
     ULibSync.syncULibPlayerBanData()
 end
 local syncBans = ulx.command("ULibSync", "ulx syncbans", ulx.syncBans, "!syncbans")
-syncBans:defaultAccess(ULib.ACCESS_OPERATOR)
+syncBans:defaultAccess(ULib.ACCESS_ADMIN)
 syncBans:help("Syncs all local ban data to the ULibSync database.")
 
 
@@ -10,5 +10,5 @@ function ulx.syncUsers(calling_ply)
     ULibSync.syncULibUsers()
 end
 local syncBans = ulx.command("ULibSync", "ulx syncusers", ulx.syncUsers, "!syncusers")
-syncBans:defaultAccess(ULib.ACCESS_OPERATOR)
+syncBans:defaultAccess(ULib.ACCESS_ADMIN)
 syncBans:help("Syncs all local users to the ULibSync database.")
