38f56e1eee3280b09a0d3768e00503399fac97e7
sunset <aidanstewart@sunsetdeveloper.com>
2021-05-25 16:18:29 -0400

    Group commands, Code cleanup, and bug fixes

--- lua/ulibsync/server/sv_config.lua
+++
@@ -1,9 +0,0 @@
-ULibSync.syncGroupsOnInit = true
-
---MySQL Settings
---You will need MySQLOO for this.
-ULibSync.ip = "example.com"
-ULibSync.username = "example"
-ULibSync.password = "password"
-ULibSync.database = "exampledb"
-ULibSync.port = 3306
